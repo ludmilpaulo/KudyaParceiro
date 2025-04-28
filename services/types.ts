4b77129b2f50fcaa04ac993f7fd145197c69945f
ludmil <ludmilpaulo@gmail.com>
2025-04-29 00:23:50 +0200

    eas

--- services/types.ts
+++ services/types.ts
@@ -1,5 +1,5 @@
 //export const baseAPI: string = "http://127.0.0.1:8000";
-export const baseAPI: string = "https://www.kudya.shop";
+export const baseAPI: string = "https://kudya.pythonanywhere.com";
 
 //export const baseAPI: string = "http://192.168.1.108:8000";
 
